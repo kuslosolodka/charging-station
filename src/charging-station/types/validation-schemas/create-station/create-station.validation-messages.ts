0ec480e6e662c1e7df24a10473c67d91444daacd
Mariia H <ma.gaiduchenko@gmail.com>
2024-03-01 22:51:32 +0200

    refactored validation messages

--- src/charging-station/types/validation-schemas/create-station/create-station.validation-messages.ts
+++ src/charging-station/types/validation-schemas/create-station/create-station.validation-messages.ts
@@ -1,19 +1,23 @@
 export const VALIDATION_MESSAGES = {
-  TITLE_INVALID_TYPE: 'Title should be a string',
-  TITLE_REQUIRED: 'Title should be present',
-  EMAIL_INVALID_TYPE: 'Email should be a string',
-  EMAIL_REQUIRED: 'Email should be present',
+  required: {
+    TITLE_REQUIRED: 'Title should be present',
+    EMAIL_REQUIRED: 'Email should be present',
+    DESCRIPTION_REQUIRED: 'Description should be present',
+    PUBLICITY_REQUIRED: 'Publicity should be present',
+    LOCATION_COORDINATES_REQUIRED: 'Coordinates should present',
+    LOCATION_COORDINATE_REQUIRED: 'Coordinate should be present',
+    LOCATION_REQUIRED: 'Location should be present',
+  },
+  invalid: {
+    TITLE_INVALID_TYPE: 'Title should be a string',
+    EMAIL_INVALID_TYPE: 'Email should be a string',
+    DESCRIPTION_INVALID_TYPE: 'Description should be a string',
+    PUBLICITY_INVALID_TYPE: 'Publicity should be set to true or false',
+    LOCATION_COORDINATES_INVALID_TYPE:
+      'Coordinates should be an array of numbers',
+    LOCATION_COORDINATE_INVALID_TYPE: 'Coordinate should be number',
+    LOCATION_INVALID_TYPE: 'Location should be object',
+  },
   EMAIL_FORMAT: 'Email should has @email.com ending',
-  DESCRIPTION_INVALID_TYPE: 'Description should be a string',
-  DESCRIPTION_REQUIRED: 'Description should be present',
-  PUBLICITY_INVALID_TYPE: 'Publicity should be set to true or false',
-  PUBLICITY_REQUIRED: 'Publicity should be present',
-  LOCATION_COORDINATES_INVALID_TYPE:
-    'Coordinates should be an array of numbers',
-  LOCATION_COORDINATES_REQUIRED: 'Coordinates should present',
-  LOCATION_COORDINATE_INVALID_TYPE: 'Coordinate should be number',
-  LOCATION_COORDINATE_REQUIRED: 'Coordinate should be present',
-  LOCATION_INVALID_TYPE: 'Location should be object',
   UNKNOWN_KEY: 'Unknown key in the body',
-  LOCATION_REQUIRED: 'Location should be present',
 } as const;
--- src/charging-station/types/validation-schemas/create-station/create-station.validation-schema.ts
+++ src/charging-station/types/validation-schemas/create-station/create-station.validation-schema.ts
@@ -5,18 +5,19 @@ export const createStationValidationSchema = z
   .object({
     title: z
       .string({
-        invalid_type_error: VALIDATION_MESSAGES.TITLE_INVALID_TYPE,
+        invalid_type_error: VALIDATION_MESSAGES.invalid.TITLE_INVALID_TYPE,
       })
       .optional(),
     email: z
       .string({
-        invalid_type_error: VALIDATION_MESSAGES.EMAIL_INVALID_TYPE,
+        invalid_type_error: VALIDATION_MESSAGES.invalid.EMAIL_INVALID_TYPE,
       })
       .email(VALIDATION_MESSAGES.EMAIL_FORMAT)
       .optional(),
     description: z
       .string({
-        invalid_type_error: VALIDATION_MESSAGES.DESCRIPTION_INVALID_TYPE,
+        invalid_type_error:
+          VALIDATION_MESSAGES.invalid.DESCRIPTION_INVALID_TYPE,
       })
       .optional(),
     location: z
@@ -26,46 +27,49 @@ export const createStationValidationSchema = z
             [
               z.number({
                 invalid_type_error:
-                  VALIDATION_MESSAGES.LOCATION_COORDINATE_INVALID_TYPE,
+                  VALIDATION_MESSAGES.invalid.LOCATION_COORDINATE_INVALID_TYPE,
                 required_error:
-                  VALIDATION_MESSAGES.LOCATION_COORDINATE_REQUIRED,
+                  VALIDATION_MESSAGES.required.LOCATION_COORDINATE_REQUIRED,
               }),
               z.number({
                 invalid_type_error:
-                  VALIDATION_MESSAGES.LOCATION_COORDINATE_INVALID_TYPE,
+                  VALIDATION_MESSAGES.invalid.LOCATION_COORDINATE_INVALID_TYPE,
                 required_error:
-                  VALIDATION_MESSAGES.LOCATION_COORDINATE_REQUIRED,
+                  VALIDATION_MESSAGES.required.LOCATION_COORDINATE_REQUIRED,
               }),
             ],
             {
               invalid_type_error:
-                VALIDATION_MESSAGES.LOCATION_COORDINATES_INVALID_TYPE,
-              required_error: VALIDATION_MESSAGES.LOCATION_COORDINATES_REQUIRED,
+                VALIDATION_MESSAGES.invalid.LOCATION_COORDINATES_INVALID_TYPE,
+              required_error:
+                VALIDATION_MESSAGES.required.LOCATION_COORDINATES_REQUIRED,
             }
           ),
         },
-        { invalid_type_error: VALIDATION_MESSAGES.LOCATION_INVALID_TYPE }
+        {
+          invalid_type_error: VALIDATION_MESSAGES.invalid.LOCATION_INVALID_TYPE,
+        }
       )
       .optional(),
     isPublic: z.boolean({
-      invalid_type_error: VALIDATION_MESSAGES.PUBLICITY_INVALID_TYPE,
-      required_error: VALIDATION_MESSAGES.PUBLICITY_REQUIRED,
+      invalid_type_error: VALIDATION_MESSAGES.invalid.PUBLICITY_INVALID_TYPE,
+      required_error: VALIDATION_MESSAGES.required.PUBLICITY_REQUIRED,
     }),
   })
   .strict(VALIDATION_MESSAGES.UNKNOWN_KEY)
   .refine((data) => (data.isPublic ? data.title : true), {
-    message: VALIDATION_MESSAGES.TITLE_REQUIRED,
+    message: VALIDATION_MESSAGES.required.TITLE_REQUIRED,
     path: ['title'],
   })
   .refine((data) => (data.isPublic ? data.email : true), {
-    message: VALIDATION_MESSAGES.EMAIL_REQUIRED,
+    message: VALIDATION_MESSAGES.required.EMAIL_REQUIRED,
     path: ['email'],
   })
   .refine((data) => (data.isPublic ? data.description : true), {
-    message: VALIDATION_MESSAGES.DESCRIPTION_REQUIRED,
+    message: VALIDATION_MESSAGES.required.DESCRIPTION_REQUIRED,
     path: ['description'],
   })
   .refine((data) => (data.isPublic ? data.location : true), {
-    message: VALIDATION_MESSAGES.LOCATION_REQUIRED,
+    message: VALIDATION_MESSAGES.required.LOCATION_REQUIRED,
     path: ['location'],
   });
